3599ae8bbf0fb26b6d9bddac4d71fc5f3e3e2f24
Software Fejleszto 13 <13SZFT@SOBL.local>
2022-02-09 10:23:26 +0100

    Pontozo

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Szazalek valami = new Szazalek();
+            valami.szazzalek(250, 5); //1. Érték a maximális pontszám, 2. érték az osztályfő.
+
+        }
+    }
+}
---
+++ ConsoleApp4/Szazalek.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Szazalek
+    {
+        public double szazzalek(double pont_maximum, int letszam)
+        {
+            double szazalek = 0;
+            //A program létrehozz egy txt fájlt is a mappában.
+            StreamWriter iro = new StreamWriter("eredmenyek.txt", true, Encoding.UTF8);
+            for (int i = 1; i < letszam; i++)
+            {
+                Console.Write("Írja be az elért pontszámot: ");
+                double pontszam = double.Parse(Console.ReadLine());
+                double oszto = pont_maximum / 100;
+                szazalek = pontszam / oszto;
+                Console.WriteLine(szazalek);
+                if (szazalek <= 30)
+                {
+                    Console.WriteLine("Elégtelen");
+                    iro.WriteLine("Az " + i + ". Tanuló pontszáma: " + szazalek);
+                    iro.WriteLine("Az " + i + ". Tanuló nem ment át a vizsgán.");
+                    iro.WriteLine("\n");
+                }
+                else if (szazalek <= 40)
+                {
+                    Console.WriteLine("Elégséges");
+                    iro.WriteLine("Az " + i + ". Tanuló pontszáma: " + szazalek);
+                    iro.WriteLine("Az " + i + ". Tanuló át ment a vizsgán.");
+                    iro.WriteLine("\n");
+                }
+                else if (szazalek <= 60)
+                {
+                    Console.WriteLine("Közepes");
+                    iro.WriteLine("Az " + i + ". Tanuló pontszáma: " + szazalek);
+                    iro.WriteLine("Az " + i + ". Tanuló át ment a vizsgán.");
+                    iro.WriteLine("\n");
+                }
+                else if (szazalek <= 79)
+                {
+                    Console.WriteLine("Jó");
+                    iro.WriteLine("Az " + i + ". Tanuló pontszáma: " + szazalek);
+                    iro.WriteLine("Az " + i + ". Tanuló át ment a vizsgán.");
+                    iro.WriteLine("\n");
+                }
+                else
+                {
+                    Console.WriteLine("Kiváló");
+                    iro.WriteLine("Az " + i + ". Tanuló pontszáma: " + szazalek);
+                    iro.WriteLine("Az " + i + ". Tanuló át ment a vizsgán.");
+                    iro.WriteLine("\n");
+                }
+            }
+            iro.Flush();
+            iro.Close();
+            return szazalek;
+        }
+    }
+}
